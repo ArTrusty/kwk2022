59f1f4a846e39ca65e83b5cd72a86e72a2ac1821
ArTrusty <110997809+ArTrusty@users.noreply.github.com>
2022-08-12 15:00:53 -0400

    ToDoList iteration 0 Completed

---
+++ kwk2022/toDoList/toDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by scholar on 21/05/1401 AP.
+//
+
+import UIKit
+
+class ToDoClass {
+    var description = ""
+    var important = false
+    
+}
